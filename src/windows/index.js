426b64e6103e0d20546b119ec2385dcd70479aa2
rohit1verma <vermarohit7780@gmail.com>
2025-12-25 17:48:29 +0530

    text feature implemented

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,5 +2,6 @@ import Terminal from "./Terminal";
 import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
+import Text from "./Text";
 
-export { Terminal, Safari, Resume, Finder };
+export { Terminal, Safari, Resume, Finder, Text };
