7091ccd111506dfc68a7d912b004317287ace417
rohit1verma <vermarohit7780@gmail.com>
2025-12-25 18:07:00 +0530

    implemented image feature

--- src/windows/index.js
+++ src/windows/index.js
@@ -3,5 +3,6 @@ import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
 import Text from "./Text";
+import Image from "./Image"
 
-export { Terminal, Safari, Resume, Finder, Text };
+export { Terminal, Safari, Resume, Finder, Text, Image };
